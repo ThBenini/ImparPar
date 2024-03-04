e8023bcc2c8d29c60bc05ba0adc4d5d1eac43ea8
Thiago Benini <139355804+ThBenini@users.noreply.github.com>
2024-03-04 15:23:12 -0300

    Add files via upload

---
+++ Imparpar.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Imparpar {
+    public static void main(String[] args){
+
+        int n1, n2, resultado;
+
+        Scanner ler = new Scanner(System.in);
+
+
+        System.out.println("Número: ");
+        n1 = ler.nextInt();
+
+        if (n1 % 2 == 0)
+        {
+            System.out.println("Número PAR");
+        }
+
+        else {
+            System.out.println("Número ÍMPAR");
+        }
+    }
+
+}
